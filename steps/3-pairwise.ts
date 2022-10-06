48efcf22f0a8a6fdcfb5e9ffb1ef2464f29905b8
ASobiechowski <30715982+ASobiechowski@users.noreply.github.com>
2022-10-06 13:18:56 +0200

    1 file modified

--- steps/3-pairwise.ts
+++ steps/3-pairwise.ts
@@ -4,7 +4,7 @@ import { map, pairwise, startWith, take } from 'rxjs/operators';
 export const $pairwiseObservable = interval(1000)
 .pipe(
   map(m=>Math.round(m/3)),
-  startWith(null),
+  //startWith(null),
   pairwise(), 
   take(10),
   //map(([oldFormValue, newFormValue]) => oldFormValue ==newFormValue)
