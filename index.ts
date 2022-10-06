bb490f67daf1872b054b27687db82c27eccd047c
ASobiechowski <30715982+ASobiechowski@users.noreply.github.com>
2022-10-06 12:34:01 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -5,11 +5,3 @@ import * as steps from './steps';
 const $intervalObservable=steps.$intervalObservable
   .subscribe(console.log);
   $intervalObservable.unsubscribe();
-
-  const $takeObservable=steps.$takeObservable
-  .subscribe(console.log);
-  $takeObservable.unsubscribe();
-
-const $pairwiseObservable=steps.$pairwiseObservable
-  .subscribe(console.log);
-  $pairwiseObservable.unsubscribe();
